ef07051fd49bd1697717f503ab707425031d297b
deanaynslie <dena.garcia.up@phinmaed.com>
2025-07-23 16:30:05 +0800

    made minor changes

--- weight_converter.py
+++ weight_converter.py
@@ -1,7 +1,7 @@
 is_running = True
 while(is_running):
     try:
-        option_input = int(input("\nChoose option [1] Kilogram to Pound [2] Pound to Kilogram\nOption:"))
+        option_input = int(input("\nChoose option [1] Kilogram to Pound [2] Pound to Kilogram [3] Exit\nOption:"))
         if option_input == 1:
             weight_input = float(input("\nEnter Weight(kg): "))
             kilogram_to_pound = round(weight_input * 2.205, 1)
@@ -10,6 +10,10 @@ while(is_running):
             weight_input = float(input("\nEnter Weight(lbs): "))
             pound_to_kilogram = round(weight_input / 2.205,1)
             print(f"Converted to {pound_to_kilogram}kg")
+        elif option_input == 3:
+            break
+        else: 
+            print("Invalid input. Please try again.")
     except Exception as e:
         print("Invalid input. Please try again.")
         continue
